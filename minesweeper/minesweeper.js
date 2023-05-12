0a9fef73725659fa69c7dcbe3745f3144cd3f2a4
kyzym <kyzym1@gmail.com>
2023-05-12 07:26:45 +0200

    feat: add number colors  05/12/2023 07:26:45

--- minesweeper/minesweeper.js
+++ minesweeper/minesweeper.js
@@ -47,7 +47,7 @@ for (let i = 0; i < SIZE; i++) {
     const cellContent = document.createElement('span');
 
     cell.classList.add('cell');
-    cellContent.classList.add('sell-content');
+    cellContent.classList.add('cell-content');
     cell.appendChild(cellContent);
     cell.addEventListener('click', () => {
       if (gameOver) {
@@ -69,7 +69,9 @@ for (let i = 0; i < SIZE; i++) {
       } else {
         cell.classList.remove('cell');
         cell.classList.add('number');
+
         cellContent.textContent = board[i][j];
+        setColor(cellContent, board[i][j].toString());
         openedCells.add(`${i},${j}`);
 
         if (openedCells.size === SIZE * SIZE - MINES) {
@@ -83,8 +85,37 @@ for (let i = 0; i < SIZE; i++) {
       }
     });
 
-    container.appendChild(cell); // This line was missing
+    container.appendChild(cell);
   }
 }
 
 document.body.appendChild(container);
+
+function setColor(cellContent, number) {
+  switch (number) {
+    case '1':
+      cellContent.style.color = 'blue';
+      break;
+    case '2':
+      cellContent.style.color = 'green';
+      break;
+    case '3':
+      cellContent.style.color = 'red';
+      break;
+    case '4':
+      cellContent.style.color = 'purple';
+      break;
+    case '5':
+      cellContent.style.color = 'maroon';
+      break;
+    case '6':
+      cellContent.style.color = 'turquoise';
+      break;
+    case '7':
+      cellContent.style.color = 'black';
+      break;
+    case '8':
+      cellContent.style.color = 'gray';
+      break;
+  }
+}
